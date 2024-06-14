17d6eafc5889bdabd14510342eff58ef03d7a6ae
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-14 21:04:25 +0900

    :recycle: :: LocalTime Format함수 1자리수 LocalTime Minute 10의 자리에  0붙여서 처리하게 변경
    :: LocalTime Format함수 1자리수 LocalTime Minute 10의 자리에  0붙여서 처리하게 변경

--- app/src/main/java/com/assignment/ykgiants_assignment/chat/util/timeFormater.kt
+++ app/src/main/java/com/assignment/ykgiants_assignment/chat/util/timeFormater.kt
@@ -9,7 +9,8 @@ fun LocalTime.toKoreanFormat(): String {
     val hour = this.hour
     val minute = this.minute
     val amPm = if (hour < 12) "오전" else "오후"
-    val formattedHour = if (hour % 12 == 0) 12 else hour % 12 // 12시간 형식으로 변경
+    val formattedHour = if (hour % 12 == 0) 12 else hour % 12
+    val formattedMinute = if (minute < 10) "0$minute" else minute
 
-    return "$amPm $formattedHour:$minute"
+    return "$amPm $formattedHour:$formattedMinute"
 }
