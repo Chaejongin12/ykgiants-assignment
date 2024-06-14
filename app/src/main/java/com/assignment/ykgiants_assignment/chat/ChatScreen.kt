aa81030f892d6c5f4226a7926117c7890521795a
Chaejongin12 <s22055@gsm.hs.kr>
2024-06-14 20:47:47 +0900

    :bug: :: imePadding을 적용하여 TextField가 Focused됐을때 다른 Composable이 올라오는 것 Fix
    :: imePadding을 적용하여 TextField가 Focused됐을때 다른 Composable이 올라오는 것 Fix

--- app/src/main/java/com/assignment/ykgiants_assignment/chat/ChatScreen.kt
+++ app/src/main/java/com/assignment/ykgiants_assignment/chat/ChatScreen.kt
@@ -2,6 +2,7 @@ package com.assignment.ykgiants_assignment.chat
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,35 +49,33 @@ fun ChatScreen(
         }
     }
 
-    Surface(
+    Column(
         modifier = modifier
             .fillMaxSize()
             .clickable {
                 keyboardController?.hide()
-            },
+            }
+            .imePadding(),
     ) {
-        Column(
-            modifier = modifier.fillMaxSize(),
-        ) {
-            ChatTopBar()
+        ChatTopBar()
 
-            MessageScreen(
-                messages = messages,
-                modifier = modifier.weight(1f),
-            )
+        MessageScreen(
+            messages = messages,
+            modifier = modifier
+                .weight(1f)
+        )
 
 
-            ChatTextField(
-                placeholder = "메시지를 입력하세요",
-                onButtonClick = { text ->
-                    chatViewModel.addMessage(messages, text) { newMessages ->
-                        messages = newMessages
-                    }
-                },
-                modifier = modifier
-                    .fillMaxWidth()
-            )
-        }
+        ChatTextField(
+            placeholder = "메시지를 입력하세요",
+            onButtonClick = { text ->
+                chatViewModel.addMessage(messages, text) { newMessages ->
+                    messages = newMessages
+                }
+            },
+            modifier = modifier
+                .fillMaxWidth()
+        )
     }
 }
 
